38b9198a811a0438ee823aea8f97b756d81f5536
shivapal <shivapal@buffalo.edu>
2019-04-03 01:34:37 -0400

    Revert "Added plotting."
    This reverts commit a623ac1ce9c4957f6e5ac7af39f58927127cf865.

--- nnScript.py
+++ nnScript.py
@@ -2,24 +2,20 @@
 Neural Network Script Starts here
 '''
 from nnFunctions import *
-import time
-import matplotlib.pyplot as plt
 # you may experiment with a small data set (mnist_sample.pickle) first
 # filename = 'mnist_sample.pickle'
-# filename = 'AI_quick_draw.pickle'
-filename = 'mnist_all.pickle'
+filename = 'AI_quick_draw.pickle'
 train_data, train_label, test_data, test_label = preprocess(filename)
-# set the number of nodes in output unit
-n_class = 10
+
 #  Train Neural Network
 # set the number of nodes in input unit (not including bias unit)
 n_input = train_data.shape[1]
-# for n_hidden in (4,8,12,16,20):
 
-    # set the number of nodes in hidden unit (not including bias unit)
+# set the number of nodes in hidden unit (not including bias unit)
 n_hidden = 50
 
-
+# set the number of nodes in output unit
+n_class = 10
 
 # initialize the weights into some random matrices
 initial_W1 = initializeWeights(n_input, n_hidden)
@@ -29,45 +25,26 @@ initial_W2 = initializeWeights(n_hidden, n_class)
 initialWeights = np.concatenate((initial_W1.flatten(), initial_W2.flatten()), 0)
 
 # set the regularization hyper-parameter
-# lambdaval = 0
-x=[]
-train=[]
-test=[]
-times=[]
-for lambdaval in np.linspace(0,60,12):
-    st = time.time()
-    args = (n_input, n_hidden, n_class, train_data, train_label, lambdaval)
+lambdaval = 0
 
-    # Train Neural Network using fmin_cg or minimize from scipy,optimize module. Check documentation for a working example
-    opts = {'maxiter': 50}  # Preferred value.
+args = (n_input, n_hidden, n_class, train_data, train_label, lambdaval)
 
-    nn_params = minimize(nnObjFunction, initialWeights, jac=True, args=args, method='CG', options=opts)
-    end=time.time()
-    times.append(end-st)
-    x.append(lambdaval)
+# Train Neural Network using fmin_cg or minimize from scipy,optimize module. Check documentation for a working example
+opts = {'maxiter': 50}  # Preferred value.
 
-    W1 = nn_params.x[0:n_hidden * (n_input + 1)].reshape((n_hidden, (n_input + 1)))
-    W2 = nn_params.x[(n_hidden * (n_input + 1)):].reshape((n_class, (n_hidden + 1)))
-    print("chicken done!")
-    predicted_label = nnPredict(W1, W2, train_data)
-    train.append(100 * np.mean((predicted_label == train_label).astype(float)))
-    predicted_label = nnPredict(W1, W2, test_data)
-    test.append(100 * np.mean((predicted_label == test_label).astype(float)))
-
-plt.plot(x,times)
-plt.plot(x,train,'g',x,test,'r')
+nn_params = minimize(nnObjFunction, initialWeights, jac=True, args=args, method='CG', options=opts)
 
 # Reshape nnParams from 1D vector into W1 and W2 matrices
-# W1 = nn_params.x[0:n_hidden * (n_input + 1)].reshape((n_hidden, (n_input + 1)))
-# W2 = nn_params.x[(n_hidden * (n_input + 1)):].reshape((n_class, (n_hidden + 1)))
-# print("chicken done!")
+W1 = nn_params.x[0:n_hidden * (n_input + 1)].reshape((n_hidden, (n_input + 1)))
+W2 = nn_params.x[(n_hidden * (n_input + 1)):].reshape((n_class, (n_hidden + 1)))
+print("chicken done!")
 
 # Test the computed parameters
 
 # find the accuracy on Training Dataset
-# predicted_label = nnPredict(W1, W2, train_data)
+predicted_label = nnPredict(W1, W2, train_data)
 print('\n Training set Accuracy:' + str(100 * np.mean((predicted_label == train_label).astype(float))) + '%')
 
 # find the accuracy on Testing Dataset
-# predicted_label = nnPredict(W1, W2, test_data)
+predicted_label = nnPredict(W1, W2, test_data)
 print('\n Test set Accuracy:    ' + str(100 * np.mean((predicted_label == test_label).astype(float))) + '%')
